dd6abfff9498b66f603e140a8c2ebcf544fb1136
Joshua Mutambuki <mutambukijoshua2@gmail.com>
2024-07-16 16:26:52 +0300

    calculator

---
+++ Calculator.js
@@ -0,0 +1,36 @@
+//Add function
+function add(num1, num2){
+    return num1 + num2;
+}
+
+//Minus function
+function minus(num1, num2){
+    return num1 - num2;
+}
+
+//Multiply function
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+//Divide function
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+//Calculator function
+function calculator(num1, num2, operrator){
+    return operrator(num1, num2);
+}
+
+
+console.log(calculator(5, 4, add))
+
+
+
+
+
+
+
+
+
