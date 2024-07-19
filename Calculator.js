e44b77ee5f35d1f7207daeb2207f65c82e912c18
Joshua Mutambuki <mutambukijoshua2@gmail.com>
2024-07-19 22:57:33 +0300

    display the typed number

--- Calculator.js
+++ Calculator.js
@@ -1,10 +1,85 @@
+class calculator{
+    constructor(previousOPerandTxtEle, currentOPerandTxtEle){
+        this.previousOPerandTxtEle = previousOPerandTxtEle
+        this.currentOPerandTxtEle = currentOPerandTxtEle
+        //To clear all our inputs in the calculator
+        this.clear()
+    }
+
+    //To clear the input
+    clear(){
+        //when cleared the answer will be empty string
+        this.currentOPerand = ''
+
+        //when cleared the input will be empty string
+        this.previousOPerand = ''
+
+        //when cleared the operation sign will be undefined
+        this.operation = undefined
+    }
+
+    //Remove a single number 
+    delete(){
+
+    }
+
+    //Add the clicked number to the screen
+    appendNumber(number){
+        this.currentOPerand= number
+    }
+
+    //Choose the icon from the icon
+    chooseOperationIcon(operation){
+
+    }
+
+    //Take the inputs and compute them
+    compute(){
+
+    }
+
+    //Update values inside our screen
+    updateDisplay(){
+        this.currentOPerandTxtEle.innerText= this.currentOPerand
+    }
+}
 
 
+//The number buttons that will be inputed for calculations
+const numberButtons= document.querySelectorAll(`[data-number]`);
+
+//The operation button to specify the operation to take place
+const operationButtons= document.querySelectorAll(`[data-operation]`);
+
+//The equals button 
+const equalsButtons= document.querySelector(`[data-equals]`);
+
+//The delete button that will remove one digit from the inputs
+const deleteButton= document.querySelector(`[data-delete]`);
+
+//Clear button will erase the whole input back to default
+const allClearButton= document.querySelector(`[data-all-clear]`);
+
+//This is the input that the user typed in
+const previousOPerandTxtEle= document.querySelector(`[data-previous-operand]`);
+
+//This is the answer after operation is done
+const currentOPerandTxtEle= document.querySelector(`[data-current-operand]`);
 
 
+//
+const Calculator= new calculator(previousOPerandTxtEle, currentOPerandTxtEle);
 
-
-
+//selecting our number buttons
+//for each button add an eventlistener
+numberButtons.forEach(function(button){
+    button.addEventListener("click", function(){
+        //Adding the number to our calculator
+        Calculator.appendNumber(button.innerText)
+        //display the selected number
+        Calculator.updateDisplay()
+    })
+})
 
 
 
